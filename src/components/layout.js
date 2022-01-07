8b8809c2d549cf3401347d70be9d0af51e1f24e7
sanchezd90 <sanchezd90@gmail.com>
2022-01-07 18:54:06 -0500

    layout bug fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
-import Sidebar from "./sidebar";
+import SideBar from "./SideBar";
 import { Grid } from "@material-ui/core";
 import Cookies from "universal-cookie";
 import jwt from "jsonwebtoken";
@@ -27,10 +27,10 @@ export default function Layout({ children }) {
       <NavBar user={user?.user} setUser={setUser}/>
       <Grid container style={{marginTop:'50px'}}>
         <Grid item xs={2} style={{marginLeft:'25px',height:'100%'}}>
-          <Sidebar user={user?.user} setUser={setUser}/>
+          <SideBar user={user?.user} setUser={setUser}/>
         </Grid>
         <Grid item xs={9} style={{height:'100%'}}>{children}</Grid>
-      </Grid>
+      </Grid>      
     </div>
   );
 }
